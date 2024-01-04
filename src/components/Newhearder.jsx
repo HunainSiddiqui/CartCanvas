66fda05424c6b71050e6ed4946cf584a58a00cfd
Mohammad Hunain Siddiqui <91600069+HunainSiddiqui@users.noreply.github.com>
2024-01-04 12:21:37 +0530

    Update Newhearder.jsx

--- src/components/Newhearder.jsx
+++ src/components/Newhearder.jsx
@@ -24,17 +24,16 @@ import MoreIcon from '@material-ui/icons/MoreVert';
 import { ReactComponent as IconPersonBadgeFill } from "bootstrap-icons/icons/person-badge-fill.svg";
 import { Link } from "react-router-dom";
 import TocIcon from '@mui/icons-material/Toc';
-
-const styles = theme => ({
+import axios from "axios";
+const styles = (theme) => ({
   root: {
-    width: '100%',
+    flexGrow: 1,
   },
   grow: {
     flexGrow: 1,
   },
   menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
+    marginRight: theme.spacing(2),
   },
   title: {
     display: 'none',
@@ -49,16 +48,15 @@ const styles = theme => ({
     '&:hover': {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-    marginRight: theme.spacing.unit * 2,
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit * 3,
+      marginLeft: theme.spacing(1),
       width: 'auto',
     },
   },
   searchIcon: {
-    width: theme.spacing.unit * 9,
+    width: theme.spacing(7),
     height: '100%',
     position: 'absolute',
     pointerEvents: 'none',
@@ -68,81 +66,72 @@ const styles = theme => ({
   },
   inputRoot: {
     color: 'inherit',
-    width: '100%',
   },
   inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
+    padding: theme.spacing(1, 1, 1, 7),
     transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
     },
   },
 });
+const PrimarySearchAppBar = ({ classes }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
 
-class PrimarySearchAppBar extends React.Component {
-  state = {
-    anchorEl: null,
-    mobileMoreAnchorEl: null,
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const { state, dispatch, totalprice, settotalprice } = useCart();
+
+  const handlelogout = async () => {
+    try {
+      const res = await axios.get(
+        "https://ecommersebackend1.onrender.com/api/v1/logout"
+      );
+      if (res.status === 200) {
+        localStorage.clear();
+        dispatch({ type: "CLEAR_CART" });
+        document.cookie = "token" + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  handleProfileMenuOpen = event => {
-    this.setState({ anchorEl: event.currentTarget });
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-    this.handleMobileMenuClose();
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
   };
 
-  handleMobileMenuOpen = event => {
-    this.setState({ mobileMoreAnchorEl: event.currentTarget });
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  handleMobileMenuClose = () => {
-    this.setState({ mobileMoreAnchorEl: null });
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
   };
- 
 
-  render() {
-    const { anchorEl, mobileMoreAnchorEl } = this.state;
-    const { classes } = this.props;
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <MenuItem onClick={this.handleMenuClose}><Link className="dropdown-item" to="/account/signin">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-box-arrow-in-right"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>
+        <Link className="dropdown-item" to="/account/signin">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+            {/* SVG path for Sign In */}
+            <path
                           fill-rule="evenodd"
                           d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"
                         />
@@ -150,136 +139,124 @@ class PrimarySearchAppBar extends React.Component {
                           fill-rule="evenodd"
                           d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
                         />
-                      </svg>{" "}
-                      Sign In
-                    </Link></MenuItem>
-        <MenuItem onClick={this.handleMenuClose}><Link className="dropdown-item" to="/account/signup">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-person-circle"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+          </svg>{" "}
+          Sign In
+        </Link>
+      </MenuItem>
+
+      <MenuItem onClick={handleMenuClose}>
+        <Link className="dropdown-item" to="/account/signup">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+            {/* SVG path for Sign Up */}
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                         <path
                           fill-rule="evenodd"
                           d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                         />
-                      </svg>{" "}
-                      Sign Up
-                    </Link></MenuItem>
-                    <MenuItem onClick={this.handleMenuClose}> <Link className="dropdown-item" to="/account/profile">
-                      <IconPersonBadgeFill /> My Profile
-                    </Link></MenuItem>
-                    <MenuItem onClick={this.handleProfileMenuOpen}>
+          </svg>{" "}
+          Sign Up
+        </Link>
+      </MenuItem>
+
+      <MenuItem onClick={handleMenuClose}>
+        <Link className="dropdown-item" to="/account/profile">
+          <IconPersonBadgeFill /> My Profile
+        </Link>
+      </MenuItem>
+
+      <MenuItem onClick={handleMenuClose}>
         <Link className="dropdown-item" to="/account/orders">
-                      <IconListCheck className="text-primary" /> Orders
-                    </Link>
-        </MenuItem>
-        <MenuItem onClick={this.handleProfileMenuOpen}>
+          <IconListCheck className="text-primary" /> Orders
+        </Link>
+      </MenuItem>
+
+      <MenuItem  onClick={() => { handleMenuClose(); handlelogout(); }}>
         <Link className="dropdown-item" to="/">
-                      <IconDoorClosedFill className="text-danger" /> Logout
-                    </Link>
-        </MenuItem>
-                    
-      </Menu>
-    );
+          <IconDoorClosedFill className="text-danger" /> Logout
+        </Link>
+      </MenuItem>
+    </Menu>
+  );
 
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMobileMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <MenuItem onClick={this.handleMobileMenuClose}>
-        
-        </MenuItem>
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <IconButton color="inherit">
-            <Badge badgeContent={11} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <p>Notifications</p>
-        </MenuItem>
-        <MenuItem onClick={this.handleProfileMenuOpen}>
-          <IconButton color="inherit">
-            <AccountCircle />
-          </IconButton>
-          <p>Profile</p>
-        </MenuItem>
-        
-      </Menu>
-    );
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMobileMenuClose}>
+        {/* Placeholder item */}
+      </MenuItem>
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            
+      <MenuItem onClick={handleMobileMenuClose}>
+        <IconButton color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
 
-            <Link to="/">
-            
-              <img alt="logo" src="../../images/cart_logo1.png " 
-              style= {{width :"120px" }} />
-            </Link>
-        
-         
-       
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton color="inherit">
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
-       
-
-
-            <div >
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Link to="/">
+            <img alt="logo" src="../../images/cart_logo1.png " style={{ width: "120px" }} />
+          </Link>
+          <div>
+            {/* Search component goes here */}
             <Search />
-            </div>
-              
-            <div className={classes.grow} />
-            <div className={classes.sectionDesktop}>
-              <IconButton color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                <Link to="/cart" className="btn btn-black">
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="white" class="bi bi-basket-fill" viewBox="0 2 16 16">
-  <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z"/>
-</svg>
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+  <IconButton color="inherit">
+  <Badge badgeContent={state.items ? state.items.length : 0} color="secondary">
+      <Link to="/cart" className="btn btn-black">
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="white" className="bi bi-basket-fill" viewBox="0 2 16 16">
+          <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z" />
+        </svg>
+      </Link>
+    </Badge>
+  </IconButton>
 
-                  
-                </Link>
-                </Badge>
-              </IconButton>
-              <IconButton color="inherit">
-                <Badge color="secondary" >
-                  <Link to="/account/notification"  style={{ color: 'white', textDecoration: 'none' }}> 
-                    <NotificationsIcon  />
-                  </Link>
-                </Badge>
-              </IconButton>
-              <IconButton
-                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
-                aria-haspopup="true"
-                onClick={this.handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
-                <MoreIcon />
-              </IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-        {renderMenu}
-        {renderMobileMenu}
-      </div>
-    );
-  }
-}
+  <IconButton color="inherit">
+    <Badge color="secondary">
+      <Link to="/account/notification" style={{ color: 'white', textDecoration: 'none' }}>
+        <NotificationsIcon />
+      </Link>
+    </Badge>
+  </IconButton>
+
+  <IconButton
+    aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+    aria-haspopup="true"
+    onClick={handleProfileMenuOpen}
+    color="inherit"
+  >
+    <AccountCircle />
+  </IconButton>
+</div>
+
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+      {renderMobileMenu}
+    </div>
+  );
+};
+
 
 PrimarySearchAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
